c16cec16053fab7564b470c527aad7f6d3c342ec
VulcanMaster <dmitry.blinkov@outlook.com>
2018-06-15 13:36:08 +0300

    1. Initial Layout 2. Initial datamodel.

---
+++ AcquireNameday/AcquireNameday/NamedayModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcquireNameday
+{
+    class NamedayModel
+    {
+        public int Month { get; }
+        public int Day { get; }
+        public IEnumerable<string> Names { get; }
+
+        public NamedayModel(int month, int day, IEnumerable<string> names)
+        {
+            Month = month;
+            Day = day;
+            Names = names;
+        }
+
+        public string NameAsString => string.Join(", ", Names); // C#6 alert
+
+        //public string NamesAsString {
+        //    get { return string.Join(", ", Names); }
+        //}
+    }
+}
